55d151455d19c78bbcef513c04e08d23ff7375cd
ADJAROO OGAGA <ogagaadjaro@gmail.com>
2020-04-14 03:06:24 +0100

    removed undefined varaibles

--- src/estimator.js
+++ src/estimator.js
@@ -12,45 +12,34 @@ const covid19ImpactEstimator = (data) => {
     totalHospitalBeds: 1380614
   };
 
-  let impactEstimation = {};
-  let severeImpact = {};
-  let impact = {};
+  const impactEstimation = {};
+  const severeImpact = {};
+  const impact = {};
   impactEstimation.push({ data });
   // estimate the number of currently infected
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 10;
 
-  //estimate the number of infected peole 30 days from now
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.currentlyInfected * Math.pow(2, 10);
-  impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, 10);
+  // estimate the number of infected peole 30 days from now
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
 
   // estimated number of severe positive cases
   impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
   // number of avaiable beds for severe cases
-  impact.hospitalBedsByRequestedTime =
-    (data.totalHospitalBeds * 0.35) / impact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = (data.totalHospitalBeds * 0.35) / impact.severeCasesByRequestedTime;
 
   // estimated number for very severe positive cases
-  severeImpact.severeCasesByRequestedTime =
-    severeImpact.infectionsByRequestedTime * 0.05;
-  severeImpact.casesForVentilatorsByRequestedTime =
-    severeImpact.infectionsByRequestedTime * 0.02;
+  severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+  severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
 
   // economy loss for impact case
-  impact.dollarsInFlight =
-    impact.infectionsByRequestedTime *
-    data.region.avgDailyIncomePopulation *
-    data.region.avgDailyIncomeInUSD;
-
-  // economy loss for severe impact case
-  severeImpact.dollarsInFlight =
-    severeImpact.infectionsByRequestedTime *
-    data.region.avgDailyIncomePopulation *
-    data.region.avgDailyIncomeInUSD;
-
-  impactEstimation.push({ severeImpact: { severeImpact: severeImpact } });
-  impactEstimation.push({ impact: { currentlyInfected: currentlyInfected } });
+  impact.dollarsInFlight = impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD;
+	
+	// economy loss for severe impact case
+  severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD;
+  impactEstimation.push({ severeImpact});
+  impactEstimation.push({ impact });
   return impactEstimation;
 };
 
