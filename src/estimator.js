268f9f669d19bae8bc438295dafe2dc179d3ddce
ADJAROO OGAGA <ogagaadjaro@gmail.com>
2020-04-14 03:35:13 +0100

    fixed indentation issues

--- src/estimator.js
+++ src/estimator.js
@@ -12,38 +12,38 @@ const covid19ImpactEstimator = (data) => {
   //   totalHospitalBeds: 1380614
   // };
 
-		const impactEstimation = {};
-		const severeImpact = {};
-		const impact = {};
-				impactEstimation.push({ data });
-				// estimate the number of currently infected
-				impact.currentlyInfected = data.reportedCases * 10;
-				severeImpact.currentlyInfected = data.reportedCases * 10;
+  const impactEstimation = {};
+  const severeImpact = {};
+  const impact = {};
+    impactEstimation.push({ data });
+  // estimate the number of currently infected
+    impact.currentlyInfected = data.reportedCases * 10;
+    severeImpact.currentlyInfected = data.reportedCases * 10;
 
-				// estimate the number of infected peole 30 days from now
-				severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
-				impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
+  // estimate the number of infected peole 30 days from now
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 1024;
+    impact.infectionsByRequestedTime = impact.currentlyInfected * 1024;
 
-				// estimated number of severe positive cases
-				impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
-				// number of avaiable beds for severe cases
-		const beds = (data.totalHospitalBeds * 0.35);
-				impact.hospitalBedsByRequestedTime = beds / impact.severeCasesByRequestedTime;
+  // estimated number of severe positive cases
+    impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * 0.15;
+	// number of avaiable beds for severe cases
+  const beds = (data.totalHospitalBeds * 0.35);
+    impact.hospitalBedsByRequestedTime = beds / impact.severeCasesByRequestedTime;
 
-			// estimated number for very severe positive cases
-				severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
-				severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
+ // estimated number for very severe positive cases
+    severeImpact.severeCasesByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+    severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
 
-			// economy loss for impact case
-		const lossCalc = impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
-				impact.dollarsInFlight = lossCalc * data.region.avgDailyIncomeInUSD;
-				
-				// economy loss for severe impact case
-		const popCalc = severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
-				severeImpact.dollarsInFlight = popCalc * data.region.avgDailyIncomeInUSD;
-				impactEstimation.push({ severeImpact });
-				impactEstimation.push({ impact });
-				return impactEstimation;
+	// economy loss for impact case
+  const lossCalc = impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
+    impact.dollarsInFlight = lossCalc * data.region.avgDailyIncomeInUSD;
+	
+	// economy loss for severe impact case
+  const popCalc = severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation;
+    severeImpact.dollarsInFlight = popCalc * data.region.avgDailyIncomeInUSD;
+    impactEstimation.push({ severeImpact });
+    impactEstimation.push({ impact });
+  return impactEstimation;
 };
 
 export default covid19ImpactEstimator;
